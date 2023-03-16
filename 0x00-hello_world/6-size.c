7a531b8a081c4875565e50f7f527400f1eb18466
amine1 <medaminebouali1@gmail.com>
2023-03-16 20:33:23 +0000

    task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Description:'the program's description'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{	
+	int *p;
+	printf("%ld\n%ld\n%ld\n",sizeof(int),sizeof(long),sizeof(p));
+
+	return (0);
+}
