31b48fea1b83763d6b1c5eada8f3d71dcac8c69f
amine1 <medaminebouali1@gmail.com>
2023-03-16 23:48:36 +0000

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
+#include <string.h>
 /**
 * main - Entry point
 * Description:'the program's description'
-* Return: Always 1 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	if(1==0)
+	if (1 == 0)
 	{
 		return (0);
 	}
 	else
-	{
-		perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-		return(1);
+	{	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"), 1, stdout);
+		return (1);
 	}
 }
