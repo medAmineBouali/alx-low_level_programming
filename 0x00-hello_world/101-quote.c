8f8f69025354874b4394967341d9d5f974f1922b
amine1 <medaminebouali1@gmail.com>
2023-03-16 23:01:49 +0000

    task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Description:'the program's description'
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+	if(1==0)
+	{
+		return (0);
+	}
+	else
+	{
+		perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+		return(1);
+	}
+}
