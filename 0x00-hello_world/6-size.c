a65d6edf3c1769819a49049a5c6437c2b573744f
amine1 <medaminebouali1@gmail.com>
2023-03-16 21:02:19 +0000

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {	
-	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n",sizeof(char),sizeof(int),sizeof(long),sizeof(long long),sizeof(float));
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n", sizeof(char), sizeof(int), sizeof(long), sizeof(long long), sizeof(float));
 
 	return (0);
 }
