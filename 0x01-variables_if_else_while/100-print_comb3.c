199eac7182b95fe12bfcf0fa683be83887d4b551
amine1 <medaminebouali1@gmail.com>
2023-03-17 22:28:23 +0000

    task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  * main - Entry point
+ *   * Description :program that prints all possible *different combinations of two digits
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	int i;
+	int j;
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			putchar(i);
+			putchar (j);
+			if ((i == 56) && (j == 57))
+			{
+				break;
+			}
+			putchar(',');
+			putchar (' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
