7c57023dbcff99f61770ab0e4420bc3d51312656
amine1 <medaminebouali1@gmail.com>
2023-03-17 18:32:34 +0000

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * * main - Entry point
+ * * Description:'the program's description'
+ * * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("0123456789");
+	return (0);
+}
