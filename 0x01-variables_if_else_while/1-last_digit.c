6d3486816adf2a78b410e088e645518a8160c4e4
amine1 <medaminebouali1@gmail.com>
2023-03-17 17:41:20 +0000

    task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 /**
  * * last_digit - Entry point
  * * Description:'the fun's description
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * * main - Entry point
+ * * Description:'the program's description'
+ * * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c = 'a';
+
+	do {
+		printf("%c", c);
+		c++;
+	}  while (c != 'z');
+	return (0);
+}
