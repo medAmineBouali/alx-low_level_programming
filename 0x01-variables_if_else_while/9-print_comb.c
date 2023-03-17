c3d961c9f7802a29d9fc177d39e8cf1fd1a95b45
amine1 <medaminebouali1@gmail.com>
2023-03-17 22:26:19 +0000

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,12 @@
 */
 int main(void)
 {
-	char arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar(arr[i]);
-		if (i == 9)
+		putchar(i);
+		if (i == 57)
 		{
 			break;
 		}
