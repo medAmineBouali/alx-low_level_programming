552e037f32ea5e96359c83c834d75f6a2afd4a13
amine1 <medaminebouali1@gmail.com>
2023-03-21 15:27:46 +0000

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry, point
+ * Description: Longer description of the function
+ * section header: Section description
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
