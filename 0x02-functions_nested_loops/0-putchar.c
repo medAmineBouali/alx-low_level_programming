6cc75a179238cc2803b921b18d33ff681ee8779c
amine1 <medaminebouali1@gmail.com>
2023-03-21 15:22:33 +0000

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+#include "main.h"
+/**
+ * main - Entry, point
+ * Description: Longer description of the function
+ * section header: Section description
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 	printf("_putchar\n");
