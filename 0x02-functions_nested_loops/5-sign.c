81bc6b569ce73490301544a762042fac9bc4c83d
amine1 <medaminebouali1@gmail.com>
2023-03-21 16:52:47 +0000

    task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_sign - Entry, point
+* Description: Longer description of the function
+* @c: The character to check
+* section header: Section description
+* Return: Description of the returned value
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchart('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchart('-');
+		return (-1);
+	}
+	else
+	{
+		_putchart('0');
+		return (0);
+	}
+}
