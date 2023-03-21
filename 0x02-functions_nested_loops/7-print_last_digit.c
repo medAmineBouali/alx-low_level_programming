670d94abb583406749aa6a79cf833b97d332095e
amine1 <medaminebouali1@gmail.com>
2023-03-21 17:08:39 +0000

    task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_sign - Entry, point
+* print_last_digit - Entry, point
 * Description: Longer description of the function
 * @b: The character to print last digit of
 * section header: Section description
 * Return: Description of the returned value
 */
-int print_sign(int b)
+int print_last_digit(int b)
 {
 	int  last_digit = b % 10;
 
