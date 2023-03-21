dbc63c0ec49d1245075e381962a2fbcd53eafb0a
amine1 <medaminebouali1@gmail.com>
2023-03-21 18:39:27 +0000

    task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@
 */
 void jack_bauer(void)
 {
-	int t[4] ={0, 0, 0, 0}, h = 0, min = 0;
+	int t[4] = {0, 0, 0, 0}, h = 0, min = 0;
 
 	while (h < 24)
 	{
@@ -20,23 +20,23 @@ void jack_bauer(void)
 			_putchar(48 + t[2]);
 			_putchar(48 + t[3]);
 			_putchar('\n');
-			if(t[3] < 9)
+			if (t[3] < 9)
 				t[3]++;
 			else
 			{
 				t[2]++;
-				t[3]=0;
+				t[3] = 0;
 			}
-			min = t[2]*10 + t[3];
+			min = t[2] * 10 + t[3];
 		}
-		if(t[1] < 9)
+		if (t[1] < 9)
 			t[1]++;
 		else
 		{
 			t[0]++;
-			t[1]=0;
+			t[1] = 0;
 		}
-		h = t[0]*10 + t[1];
+		h = t[0] * 10 + t[1];
 		min = 0;
 		t[2] = 0;
 		t[3] = 0;
