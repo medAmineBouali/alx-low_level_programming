cfbfcd4153657fbec3f8d889ea9b8145eb24a614
amine1 <medaminebouali1@gmail.com>
2023-03-21 19:27:13 +0000

    task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* add - Entry, point
+* Description: Longer description of the function
+* @a: The character to add
+* @b: The character to add
+* section header: Section description
+* Return: Description of the returned value
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
