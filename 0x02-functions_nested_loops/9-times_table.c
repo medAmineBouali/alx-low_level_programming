b4731293bf16375465f5768a8591c43b6c8f947e
amine1 <medaminebouali1@gmail.com>
2023-03-21 19:24:08 +0000

    task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,14 +11,14 @@ void times_table(void)
 	int i, j;
 	char t[3] = {' ', ' ', ' '};
 
-	for (i=0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j=0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (j * i < 10)
 			{
 				t[2] = 48 + j * i;
-				if(j > 0)
+				if (j > 0)
 				{
 				_putchar(' ');
 				_putchar(' ');
@@ -28,7 +28,7 @@ void times_table(void)
 			else
 			{
 				t[2] = 48 + (j * i) % 10;
-				t[1] = 48 +((j * i) - (j * i) % 10)/10;
+				t[1] = 48 + ((j * i) - (j * i) % 10) / 10;
 				_putchar(t[0]);
 				_putchar(t[1]);
 				_putchar(t[2]);
