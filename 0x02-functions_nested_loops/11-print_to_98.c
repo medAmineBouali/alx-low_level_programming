b9b6f4722f023ba1ffa2a5cbf32d6c578116fd16
amine1 <medaminebouali1@gmail.com>
2023-03-21 20:06:28 +0000

    task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_to_98 - Entry, point
+* Description: Longer description of the function
+* @n: start
+* section header: Section description
+* Return: Description of the returned value
+*/
+void print_to_98(int n);
+{
+		while (n != 98 )
+		{
+			printf("%d ,", n);
+			if (n > 98)
+				n--;
+			else
+				n++;
+		}
+		printf("98\n");
+}
