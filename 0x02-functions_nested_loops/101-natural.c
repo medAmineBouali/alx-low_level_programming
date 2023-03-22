4d2c8b3a43842543eb28dcf88139140401c69ffb
amine1 <medaminebouali1@gmail.com>
2023-03-22 02:39:39 +0000

    task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry, point
+* Description: Longer description of the function
+* section header: Section description
+* Return: Description of the returned value
+*/
+void main(void)
+{
+	int i;
+	while (i < 1024)
+	{
+		if((i % 3) == 0 || (i % 5) == 0)
+			printf("%d",i);
+		i++;
+	}
+	printf("\n");
+}
