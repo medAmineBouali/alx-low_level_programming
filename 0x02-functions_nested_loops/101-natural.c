6642c610002c1b6707e17876e1f9e83204014a28
amine1 <medaminebouali1@gmail.com>
2023-03-22 02:44:27 +0000

    task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,10 +8,13 @@
 int main(void)
 {
 	int i=0;
+
 	while (i < 1024)
 	{
-		if((i % 3) == 0 || (i % 5) == 0)
-			printf("%d",i);
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			printf("%d", i);
+		}
 		i++;
 	}
 	printf("\n");
