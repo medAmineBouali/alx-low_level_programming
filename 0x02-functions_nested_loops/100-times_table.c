485c555c688d8f8737b3b10d4186b54445d3191a
amine1 <medaminebouali1@gmail.com>
2023-03-22 02:20:57 +0000

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,8 +12,8 @@ void print_times_table(int n)
 	int i, j;
 	char t[4] = {' ', ' ', ' ', ' '};
 
-	if (n < 0 && n > 15)
-		exit(EXIT_FAILURE);
+	if (n >= 0 && n <= 15)
+	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
@@ -53,4 +53,5 @@ void print_times_table(int n)
 			}
 			_putchar('\n');
 		}
+	}
 }
