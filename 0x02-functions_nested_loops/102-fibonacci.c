8476e319c6686f4a34a0314ce891a1375771c91f
amine1 <medaminebouali1@gmail.com>
2023-03-22 02:56:18 +0000

    task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry, point
+* Description: Longer description of the function
+* section header: Section description
+* Return: Description of the returned value
+*/
+int main(void)
+{
+	int i, a = 0, b = 1, n;
+
+	for (i = 0; i < 50; i++)
+	{
+		n = a + b;
+		printf("%d", n);
+		a = b;
+		b = n;
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
