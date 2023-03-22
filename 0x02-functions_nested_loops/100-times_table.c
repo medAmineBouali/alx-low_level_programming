10edfdba2dea7f8877afbb93d9c82fbf6de9902b
amine1 <medaminebouali1@gmail.com>
2023-03-22 01:57:51 +0000

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,11 +12,11 @@ void print_times_table(int n)
 	int i, j;
 	char t[4] = {' ', ' ', ' ', ' '};
 
-	if (n => 0 && n =< 15)
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i =< n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j =< n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				if (j * i < 10)
 				{
