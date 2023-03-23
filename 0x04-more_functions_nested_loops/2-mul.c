916955cd7170590fdca4e05a7c9de0cde0ea29f8
amine1 <medaminebouali1@gmail.com>
2023-03-23 22:37:44 +0000

    task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul- Entry, point
+* Description: Longer description of the function
+* @a: The character to check
+* @b: The character
+* section header: Section description
+* Return: Description of the returned value
+*/
+int mul(int a, int b);
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,4 @@
 int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
+int mul(int, int);
