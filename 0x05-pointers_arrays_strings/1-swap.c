ec6d98ca82c6e6808e0804f0ab615c83bfc61ecc
amine1 <medaminebouali1@gmail.com>
2023-03-27 23:27:45 +0000

    task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* void swap_int- Entry, point
+* Description: Longer description of the function
+* @a: The adress to swap
+* @b: the 2md address to swap
+* section header: Section description
+* Return: Description of the returned value
+*/
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp:
+	
+}
