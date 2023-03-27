cfeff7154982d221aa9bc6fb2f69166fff261179
amine1 <medaminebouali1@gmail.com>
2023-03-27 23:49:39 +0000

    task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_strlen- Entry, point
+* Description: Longer description of the function
+* @n: The adress to reset
+* section header: Section description
+* Return: Description of the returned value
+*/
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@
 int _puctchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
