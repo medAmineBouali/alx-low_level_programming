c5fbe57dfb3a00c0b5ad276394c370895d5fd3f8
amine1 <medaminebouali1@gmail.com>
2023-03-27 23:22:25 +0000

    task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* reset_to_98- Entry, point
+* Description: Longer description of the function
+* @n: The adress to reset
+* section header: Section description
+* Return: Description of the returned value
+*/
+void reset_to_98(int *n)
+{
+	n = 98;
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int _puctchar(char);
+void reset_to_98(int *n);
