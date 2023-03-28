3663b6272c1b5789fbfd1b447c4f56b2967afd25
amine1 <medaminebouali1@gmail.com>
2023-03-28 01:03:39 +0000

    task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+* rev_string- Entry, point
+* Description: Longer description of the function
+* @s: The string to reverse
+* section header: Section description
+* Return: Description of the returned value
+*/
+void rev_string(char *s)
+{
+	unsigned int i, j = strlen(s);
+	char tmp;
+
+	while (j != 0)
+	{
+		tmp = s[0]
+		for(i = 1; i < strlen(s) ; i++)
+		{
+			s[i] = tmp;
+			tmp = s[i];
+			s[0] = tmp;
+		}
+		j--;
+	}
+	_putchar('\n');
+}
