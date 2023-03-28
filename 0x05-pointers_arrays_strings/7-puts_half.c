27bba221c8f6dd7b38e8f26ea2c736642620c69a
amine1 <medaminebouali1@gmail.com>
2023-03-28 01:36:36 +0000

    task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+* puts_half- Entry, point
+* Description: Longer description of the function
+* @str: The string to reverse
+* section header: Section description
+* Return: Description of the returned value
+*/
+void puts_half(char *str)
+{
+	unsigned int i;
+
+	for (i = (strlen(s) + (strlem(s) % 2)) / 2; i < strlen(s); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
