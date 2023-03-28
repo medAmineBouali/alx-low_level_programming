6820dde5a68fe5958fe498539928ebccd96bd599
amine1 <medaminebouali1@gmail.com>
2023-03-28 01:24:52 +0000

    task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,7 +12,7 @@ void rev_string(char *s)
 	unsigned int i;
 	char tmp;
 
-	for(i = 0; i < (strlen(s) / 2); i++)
+	for (i = 0; i < (strlen(s) / 2); i++)
 	{
 		tmp = s[i];
 		s[i] = s[strlen(s) - i - 1];
