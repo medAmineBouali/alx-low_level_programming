9e19e4d7b4a95e95b9f2375b174214e5bd9edb54
amine1 <medaminebouali1@gmail.com>
2023-03-28 01:26:49 +0000

    task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+/**
+*puts2- Entry, point
+* Description: Longer description of the function
+* @str: string to print
+* section header: Section description
+* Return: Description of the returned value
+*/
+void _puts2(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; i < strlen(str); i+=2)
+	_putchar(str[i]);
+	_putchar('\n');
+}
