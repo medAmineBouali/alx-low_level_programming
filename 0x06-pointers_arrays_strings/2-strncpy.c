3b5b97893211527e737e9f04c7075eca89064ceb
amine1 <medaminebouali1@gmail.com>
2023-03-29 16:30:45 +0000

    task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,11 +18,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (src[i] != '\0' && i <= n)
+	do
 	{
 		dest[i] = src[i];
 		i++;
-	}
+	} while (src[i] != '\0' && i <= n);
 	dest[i] = '\0';
 	return (dest);
 }
