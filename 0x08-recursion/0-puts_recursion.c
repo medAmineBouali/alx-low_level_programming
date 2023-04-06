18c15c2c3fd8f0266dbe561bfedeb305a804d203
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-04-06 00:50:58 +0000

    task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,10 +12,10 @@ void _puts_recursion(char *s)
 {
     _putchar(*s);
     s ++;
-    if (*s != '\0') 
+    if (*s != '\0')
     {
         _puts_recursion(s);
     }
     else
-        _putchar('\n');
+    _putchar('\n');
 }
