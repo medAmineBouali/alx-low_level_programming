6418703a655fcf7e02e8330c71fdb346e74ad457
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-04-06 00:46:44 +0000

    task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+* _puts_recursion- Entry, point
+* Description: Longer description of the function
+* @s: The string to print
+* section header: Section description
+* Return: Description of the returned value
+*/
+void _puts_recursion(char *s)
+{
+    _putchar(*s);
+    s ++;
+    if (*s != '\0') 
+    {
+        _puts_recursion(s);
+    }
+}
