7274fc5d0138a25779ab74e050dbb0592524d4d6
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-04-06 01:43:50 +0000

    task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,5 @@
 include "main.h"
-/**
- * call - calculate if a number is prime recursively
- * @n: number to evaluate
- * @i: iterator
- *
- * Return: 1 if n is prime, 0 if not
- */
-int call(int n, int i)
-{
-	if (i == 1)
-		return (1);
-	if (n % i == 0 && i > 0)
-		return (0);
-	return (call(n, i - 1));
-}
-
+int checker(int n, int i)
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
@@ -25,5 +10,20 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (call(n, n - 1));
+	return (checker(n, n - 1));
+}
+/**
+ * checker - calculate if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int checker(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (checker(n, i - 1));
 }
