bb3ecb6f26715a11833cb9d04a55f7d356073f77
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-04-06 01:39:58 +0000

    task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,26 @@
 #include "main.h"
+/**
+ * call - help the _sqrt_recursion function to find the natural square
+ * @x: input 1
+ * @y: input 2
+ *
+ * Return: the natural square OR -1
+ */
+int call(int x, int y)
+{
+	if (x * x == y)
+		return (x);
+	else if (x * x > y)
+		return (-1);
+	else
+		return (call(x + 1, y));
+}
 
-int call(int x, int y);
 /**
  * _sqrt_recursion - calculates the natural square root of a number
  * @n: the input
- * Return: -1 if n does not have a natural square root
+ *
+ * Return: -1 if n does not have a natural square root, otherwise the root
  */
 int _sqrt_recursion(int n)
 {
@@ -13,24 +29,5 @@ int _sqrt_recursion(int n)
 	else if (n < 0)
 		return (-1);
 	else
-		return (call
-        (1, n));
-}
-/**
- * call
- *  - help the _sqrt_recursion function to find the natural square
- * @x: input 1
- * @y: input 2
- * Return: the natural square OR -1
- */
-int call
-(int x, int y)
-{
-	if (x * x == y)
-		return (x);
-	else if (x * x > y)
-		return (-1);
-	else
-		return (call
-        (x + 1, y));
+		return (call(1, n));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,20 @@
 include "main.h"
+/**
+ * call - calculate if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int call(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (call(n, i - 1));
+}
 
-int call
-(int n, int i);
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
@@ -10,26 +23,7 @@ int call
  */
 int is_prime_number(int n)
 {
-if (n <= 1)
-return (0);
-return (call
-(n, n - 1));
-}
-/**
- * call
- *  - calculate if a number is prime recursively
- * @n: number to evaluate
- * @i: iterator
- *
- * Return: 1 if n is prime, 0 if not
- */
-int call
-(int n, int i)
-{
-if (i == 1)
-return (1);
-if (n % i == 0 && i > 0)
-return (0);
-return (call
-(n, i - 1));
+	if (n <= 1)
+		return (0);
+	return (call(n, n - 1));
 }
