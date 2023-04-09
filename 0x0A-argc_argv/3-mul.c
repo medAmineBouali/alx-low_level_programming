664a0d983313048286476ed9872ab360c5d8204d
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-04-09 15:38:42 +0000

    task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,7 +6,7 @@
  * * Description:'the program's description'
  * * Return: Always 0 (Success)
 */
-int main(int argc,char* argv[])
+int main(int argc,char *argv[])
 {
 	printf("%d",atoi(argv[1]) * atoi(argv[2]));
 	return (0);
