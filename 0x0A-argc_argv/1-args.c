2c5720ffecd381050df1463af813d4910d6542ba
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-04-09 16:14:26 +0000

    task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * * Description:'the fun's description
  * * Return: (Success)
  */
-int main(int argc,char* argv[])
+int main(int argc,char* argv[] __attribute__ ((unused)))
 {
 	printf("%d\n", argc);
 	return (o);
