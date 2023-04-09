aeb2cb6344e5b6768e00c8f7b4f1e068250aaa0f
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-04-09 16:07:49 +0000

    task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,12 @@
 /**
  * * main - Entry point
  * * Description:'the program's description'
+ * *@argc: parametre
+ * *@argv: parametre
  * * Return: Always 0 (Success)
 */
-int main(int argc)
+int main(int argc __attribute__ ((unused)),char* argv[])
 {
-	return (argc);
+	printf("%s\n", *argv);
+	return (0);
 }
