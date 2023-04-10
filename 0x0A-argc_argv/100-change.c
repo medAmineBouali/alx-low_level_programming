59f22f23b8a02f3a0e0a01a4bd54a8c8cda313e4
amine1 <medaminebouali1@gmail.com>
2023-04-10 13:46:37 +0000

    task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * * main - Entry point
+ * * Description:'the program's description'
+ * *@argc: parametre
+ * *@argv: parametre
+ * * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	int cents = atoi(argv[1]), i = 1;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		i++;
+	}
+	printf("%d\n", i);
+	return (0);
+}
