066967728ae2baab332d42b12a7a997763f98501
amine1 <medaminebouali1@gmail.com>
2023-04-11 03:35:41 +0000

    mylibrary

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+* _atoi- Entry, point
+* Description: Longer description of the function
+* @s: the string to copy from
+* section header: Section description
+* Return: Description of the returned value
+*/
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * sign);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _abs - Entry, point
+* Description: Longer description of the function
+* @n: The character to check
+* section header: Section description
+* Return: Description of the returned value
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
