83c2841c7f3770d204c5550a88badd826bb2998c
amine1 <medaminebouali1@gmail.com>
2023-04-12 01:13:35 +0000

    task

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,13 +4,12 @@
 * free_grid - frees a 2 dimensional grid
 * * @grid: first string
 * @height: second string
-* * Return: a pointer to the array, or NULL if it fails
-**/
+*/
 void free_grid(int **grid, int height)
 {
 	int a;
 
-	for (a = 0; i < height; a++)
+	for (a = 0; a < height; a++)
 		free(grid[a]);
 	free(grid);
 }
