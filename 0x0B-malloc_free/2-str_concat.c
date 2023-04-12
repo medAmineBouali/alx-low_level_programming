59184f426a893313a7550518365f6ff6df77c57c
amine1 <medaminebouali1@gmail.com>
2023-04-12 13:03:03 +0000

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,10 +8,10 @@
 **/
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, lent, len2, len;
+	int i, j, len1, len2, len;
 	char *result;
 
-	lent = len2 = 0;
+	len1 = len2 = 0;
 	if (s1 != NULL)
 	{
 		i = 0;
@@ -28,7 +28,7 @@ char *str_concat(char *s1, char *s2)
 	result = (char *)malloc(sizeof(char) * (len + 1));
 	if (result == NULL)
 		return (NULL);
-	for (i = 0; i < lent; i++)
+	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 	for (j = 0; j < len2; j++, i++)
 		result[i] = s2[j];
