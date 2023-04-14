b512079318bffcdf467e2f5ca0f14d7d6dd742dd
amine1 <medaminebouali1@gmail.com>
2023-04-14 01:42:41 +0000

    task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/* 
+ * malloc_checked - memory allocation using malloc
+ * @b: number of bites
+ * Return: pointer to allocated memory.
+ */
+ void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
