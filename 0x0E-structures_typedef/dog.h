a9e3cbe225030a97255fbb079038e0a42d6396b5
amine1 <medaminebouali1@gmail.com>
2023-04-18 02:32:03 +0000

    task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,10 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void free_dog(dog_t *d);
 /**
 * dog_t - typedef for struct dog
 */
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
