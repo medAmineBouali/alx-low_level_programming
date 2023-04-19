fda7849808d93cf6cf852eabdb00093a081003e4
amine1 <medaminebouali1@gmail.com>
2023-04-19 22:08:36 +0000

    task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int n, i = 0;
 
-	if (array == NULL || action == NULL || size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 	while (i <= size)
 	{
-		n = comp(array[i]);
+		n = cmp(array[i]);
 		if (n == 1)
 			break;
 		i++;
