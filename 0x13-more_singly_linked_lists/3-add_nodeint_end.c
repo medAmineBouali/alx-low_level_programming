4b97babfdff935ca9ba08ac6869730432caea31c
amine1 <medaminebouali1@gmail.com>
2023-04-30 17:10:27 +0000

    task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *last = *head; 
 
-	if (!new || !last)
+	if (!new)
 		return (NULL);
 	if (*head)
 	{
@@ -24,5 +24,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 		*head = new;
 	new->n = n;
+	new->next = NULL;
 	return (new);
 }
