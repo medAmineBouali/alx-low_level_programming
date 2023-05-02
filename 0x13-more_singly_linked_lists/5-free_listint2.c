f9b78cf7419f5b86710dc9e4a3d2878ee94b600e
amine1 <medaminebouali1@gmail.com>
2023-05-02 22:11:45 +0000

    task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: the lists head.
+ *.
+ * Return: returns nothing.
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head)
+	{
+		current = (*head)->next;
+		free(*head);
+		*head = current;
+	}
+	*head = NULL;
+}
