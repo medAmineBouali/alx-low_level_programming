8968e39aae4756d8fcd7c92dedc8167b11c53f49
amine1 <medaminebouali1@gmail.com>
2023-05-03 00:09:44 +0000

    task

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,7 +10,7 @@ int pop_listint(listint_t **head)
 	const int b = (*head)->n;
 	listint_t *tmp = (*head)->next;
 
-	if (*head == NULL)
+	if (!*head || !head)
 		return (0);
 	free(*head);
 	*head = tmp;
