c8a33b0b8afa419502062bfae2ae5050d3049a57
amine1 <medaminebouali1@gmail.com>
2023-05-03 00:38:33 +0000

    task

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 int pop_listint(listint_t **head);
