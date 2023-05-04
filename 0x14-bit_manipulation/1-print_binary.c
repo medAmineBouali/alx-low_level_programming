bbdb0fcb76e7af35bd2e2bfbed73e0b555ca4e0a
amine1 <medaminebouali1@gmail.com>
2023-05-04 16:31:11 +0000

    task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_binary - converts binary to unsigned int.
+ * @b: number in binary.
+ *
+ * Return: unsigned int.
+ */
+void print_binary(unsigned long int n)
+{
+	char byte;
+	int i, j, b = 0;
+
+	for (i = sizeof(unsigned long int) - 1; i >= 0; i--)
+	{
+		byte = ((char*)&n)[i];
+		for (j = 8; j >= 0; j--)
+		{
+			b = b + ((byte >> j) & 1);
+			if(b > 0)
+				printf("%d",(byte >> j) & 1);
+		}
+	}
+}
