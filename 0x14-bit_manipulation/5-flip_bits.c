61280c0438741b22bfec129191e2f9261305a617
amine1 <medaminebouali1@gmail.com>
2023-05-05 01:22:19 +0000

    task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * flip_bits - returns the number of bits to flip n to m.
+ * @n: number in decimal.
+ * @m: second number.
+ *
+ * Return: number of bits to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i = n ^ m; j = 0;
+
+	while (i > 0)
+	{
+		j += (i & 1);
+		i >> 1;
+	}
+	return (j);
+}
