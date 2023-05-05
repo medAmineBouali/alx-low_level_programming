2948a6994333a99054159dbc94b82ae28aaf4ffa
amine1 <medaminebouali1@gmail.com>
2023-05-05 01:12:02 +0000

    task

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 
 int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
+int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
