a245b0c7b969c05c6ea8cfc1753c2e6cd4e85b0e
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-07-06 14:20:16 +0100

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,26 +10,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
     size_t n;
     hash_table_t *hash_t = NULL;
-    hash_node_t *head = NULL;
-    hash_node_t *new_node;
-    head = (hash_node_t *)malloc(sizeof(hash_node_t));
+    hash_node_t *array = NULL;
     hash_t = (hash_table_t *)malloc(sizeof(hash_table_t));
+    array = (hash_node_t *)malloc(sizeof(hash_node_t));
 
 
-    if(!head || !hash_t)
+    if(!array || !hash_t)
     {
         fprintf(stderr, "could not allocate memory");
         return(NULL);
     }
-    for (n = 0; n < size; n++)
-    {
-        new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
-        if(!new_node)
-    {
-        fprintf(stderr, "could not allocate new memory");
-        return(NULL);
-    }
-        head->next = new_node;
-    }
+    hash_t->array = array;
     return(hash_t);
 }
